33d710baeaa65bf5e403303933a190966d697d76
TanyaEmka <tanyaemelyanova2015@gmail.com>
2023-04-20 22:01:19 +0300

    add header-style

--- musblogs/src/App.tsx
+++ musblogs/src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header/Header';
+import "./styles/base.scss";
 
 function App() {
   return (
-    <div>
-      <Header />
+    <div className="content_body">
+      <BrowserRouter basename="/">
+        <Header />
+        <div className='dynamic_content'>
+          <main>
+            <Routes>
+              <Route path="/" element={<div>MainPage</div>} />
+              <Route path="/login" element={<div>Login</div>} />
+              <Route path="/registration" element={<div></div>} />
+              <Route path="/post/:id" element={<div></div>} />
+              <Route path="/" element={<div></div>} />
+              </Routes>    
+          </main>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- musblogs/src/components/Header/Header.tsx
+++ musblogs/src/components/Header/Header.tsx
@@ -1,9 +1,44 @@
 import React, { useEffect } from "react";
+import "./Header.scss";
+import "../../styles/inputs.scss";
+import search_feed from "../../images/header/search_feed.svg";
+import notific from "../../images/header/notifications.svg";
+import search from "../../images/header/search.svg";
+import user_page from "../../images/header/user_page.svg";
+import logout from "../../images/header/logout.svg";
 
 export const Header: React.FC = () => {
     return (
         <header>
-            <p>It's header</p>
+            <div className="center_content">
+                <div>
+                    <div className="header_img">
+                        <img src={search_feed} alt="search_feed" />
+                    </div>
+                    <div className="header_img">
+                        <img src={notific} alt="notifications" />
+                    </div>
+                    <div className="search_form">
+                        <div>
+                            <input 
+                                className="search_input"
+                                type="text"
+                            />
+                        </div>
+                        <div className="header_img">
+                            <img src={search} alt="search" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="header_img">
+                        <img src={user_page} alt="userpage" />
+                    </div>
+                    <div className="header_img">
+                        <img src={logout} alt="logout" />
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
